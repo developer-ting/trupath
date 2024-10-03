e02490a3c2434393e68ea594f9dbcb5c5b7211c1
NikhileshGurrapu13 <nikhilesh.gurrapu@ting.in>
2024-10-03 18:41:53 +0530

    wip trupath

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,6 +12,12 @@
 import styles from "@/styles/components/Footer.module.scss";
 
 // IMAGES //
+import Logo from "../../public/img/footer_logo.svg";
+import linkedIn from "../../public/img/icons/social/linkedin.svg";
+import FB from "../../public/img/icons/social/fb.svg";
+import Twitter from "../../public/img/icons/social/twitter.svg";
+import Insta from "../../public/img/icons/social/insta.svg";
+import MadeByTing from "../../public/img/icons/social/MadeByTing.svg";
 
 // DATA //
 
@@ -19,7 +25,38 @@ import styles from "@/styles/components/Footer.module.scss";
 export default function Footer() {
 	return (
 		<footer className={`${styles.main_footer}`}>
-			<p>© {new Date().getFullYear()} Copyright</p>
+			<div className="container">
+				<div className={`${styles.footer_upper}`}>
+					<div className={`${styles.LogoBx}`}>
+						<img src={Logo.src} alt="Social Icons" />
+					</div>
+					<div className={`${styles.LogoBx}`}></div>
+				</div>
+				<div className={`${styles.footer_lower}`}>
+					<div className={`${styles.CopyrightBx}`}>
+						<p className="text_20">© {new Date().getFullYear()} trupath</p>
+					</div>
+					<div className={`${styles.SocialBx}`}>
+						<div className={`${styles.IconBx}`}>
+							<img src={linkedIn.src} alt="Social Icons" />
+						</div>
+						<div className={`${styles.IconBx}`}>
+							<img src={Twitter.src} alt="Social Icons" />
+						</div>
+						<div className={`${styles.IconBx}`}>
+							<img src={FB.src} alt="Social Icons" />
+						</div>
+						<div className={`${styles.IconBx}`}>
+							<img src={Insta.src} alt="Social Icons" />
+						</div>
+					</div>
+					<div className={`${styles.MadeByTing}`}>
+						<div className={`${styles.IconBx}`}>
+							<img src={MadeByTing.src} alt="Social Icons" />
+						</div>
+					</div>
+				</div>
+			</div>
 		</footer>
 	);
 }
---
+++ src/sections/FieldsSec.js
@@ -0,0 +1,58 @@
+// MODULES //
+
+// COMPONENTS //
+
+// SECTIONS //
+
+// PLUGINS //
+
+// UTILS //
+
+// STYLES //
+import styles from "@/styles/sections/FieldsSec.module.scss";
+
+// IMAGES //
+import BowlImg from "../../public/img/home/BowlImg.png";
+
+// DATA //
+
+/** FieldsSec Section */
+export default function FieldsSec() {
+	return (
+		<section className={styles.FieldsSec}>
+			<div className="container">
+				<div className={`${styles.FlexMain}`}>
+					<div className={`${styles.TextBx}`}>
+						<h2 className="text_50">
+							From the fields to
+							<br className="hidden_xs" /> our factory to you
+						</h2>
+						<h5 className="text_40">फसल से रसोई तक</h5>
+						<p className="text_20">
+							We take pride in manufacturing our products in-house at our
+							state-of-the-art, fully automatic facility. This allows us to maintain
+							stringent control over.
+						</p>
+						<p className="text_20">
+							By cutting out intermediaries, we’re able to pass the cost savings
+							directly to our customers!
+						</p>
+					</div>
+					<div className={`${styles.ImgBx}`}>
+						<div className={`${styles.FieldItem}`}>
+							<div className={`${styles.ItemImg}`}>
+								<img src={BowlImg.src} alt="Social Icons" className="width_100" />
+							</div>
+							<div className={`${styles.ItemTxt}`}>
+								<p className="text_20 f_w_m">Quality</p>
+								<p className="text_20">
+									Ensuring our products meet the highest standards
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
